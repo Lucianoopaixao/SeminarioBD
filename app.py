4f0ffb181ecf4dd814048f35833aa92a5112b1cd
Ana Raquel <123raquelana321@gmail.com>
2026-05-31 19:36:36 -0300

    .

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from repositories import MovieRepository
 def main():
     print("Iniciando Sistema de Recomendação de Filmes...\n")
     
-    # 1. Instancia a conexão com o banco
+    # Instancia a conexão com o banco
     db = Neo4jConnection()
     
     try:
-        # 2. Instancia o repositório injetando a conexão do banco
+        
         repo = MovieRepository(db.get_driver())
         
-        usuario_alvo = "Luciano" # Substitua pelo nome que quiser testar
+        usuario_alvo = "Luciano" # nome pra teste
         
-        # 3. Chama a função de recomendação colaborativa
+       
         print(f"--- Recomendações Colaborativas para {usuario_alvo} ---")
         recomendacoes_colab = repo.recomendar_colaborativo(usuario_alvo)
         
@@ -22,7 +22,7 @@ def main():
             
         print("\n----------------------------------------------------\n")
         
-        # 4. Chama a função de recomendação por conteúdo
+       
         print(f"--- Recomendações por Conteúdo para {usuario_alvo} ---")
         recomendacoes_conteudo = repo.recomendar_por_conteudo(usuario_alvo)
         
@@ -33,7 +33,7 @@ def main():
         print(f"Ocorreu um erro na aplicação: {e}")
         
     finally:
-        # 5. Garante que o banco será fechado ao final do programa
+        
         db.close()
         print("\nConexão encerrada com sucesso.")
 
